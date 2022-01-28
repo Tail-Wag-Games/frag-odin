a1162b14a653418523914a826c13c4b262c1538b
carterza <carterza@gmail.com>
2022-01-27 20:39:33 -0600

    adding deboost.context as submodule in prep for working on job system

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 @echo off
 
 if exist ".\vendor\sokol\sokol.lib" if exist ".\vendor\sokol\sokold.lib" goto :CONTINUE
-:DEPS
+:SOKOL
 call .\vendor\sokol\build.bat
+:CONTINUE
+if exist ".\vendor\deboost.context\fcontext.lib" if exist ".\vendor\deboost.context\fcontextd.lib" goto :CONTINUE
+:SOKOL
+call .\vendor\deboost.context\build.bat
 :CONTINUE
 odin build src/main.odin -out:frag.exe
 frag %*
